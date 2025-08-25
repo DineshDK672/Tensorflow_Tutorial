ce1d8af63a596e5041a08da2a40808d9c729f525
Dinesh Kumar <dineshdk672000@gmail.com>
2025-08-25 17:32:28 -0500

    CNN commit 1

---
+++ CNN.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.datasets import cifar10
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype("float32")/255.0
+x_test = x_test.astype("float32") / 255.0
+
+# Sequential Model
+# modelSeq = keras.Sequential(
+#     [
+#         keras.Input(shape=(32, 32, 3)),
+#         layers.Conv2D(32, 3, padding="valid", activation='relu'),
+#         layers.MaxPooling2D(pool_size=(2, 2)),
+#         layers.Conv2D(64, 3, activation='relu'),
+#         layers.MaxPooling2D(),
+#         layers.Conv2D(128, 3, activation='relu'),
+#         layers.Flatten(),
+#         layers.Dense(64, activation='relu'),
+#         layers.Dense(10),
+#     ]
+# )
+
+# print(modelSeq.summary())
+
+# modelSeq.compile(
+#     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#     optimizer=keras.optimizers.Adam(learning_rate=3e-4),
+#     metrics=["accuracy"],
+# )
+
+# modelSeq.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
+# modelSeq.evaluate(x_test, y_test, batch_size=64, verbose=2)
+
+# Functional Model
+
+
+def myModelFun():
+    inputs = keras.Input(shape=(32, 32, 3))
+    x = layers.Conv2D(32, 3)(inputs)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(64, 5)(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Conv2D(128, 3)(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(64, activation='relu')(x)
+    outputs = layers.Dense(10)(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+modelFun = myModelFun()
+print(modelFun.summary())
+
+modelFun.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(learning_rate=3e-4),
+    metrics=["accuracy"],
+)
+
+modelFun.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
+modelFun.evaluate(x_test, y_test, batch_size=64, verbose=2)
--- neural_networks.py
+++ neural_networks.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.datasets import mnist
+from keras import layers
+from keras.datasets import mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(-1, 28*28).astype("float32")/255.0
