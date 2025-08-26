91016b0c622732bdfb7a9a5d1edaed245f72810e
Dinesh Kumar <dineshdk672000@gmail.com>
2025-08-26 15:01:21 -0500

    CNN commit 2

--- CNN.py
+++ CNN.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers
+from keras import layers, regularizers
 from keras.datasets import cifar10
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -8,48 +8,57 @@ x_train = x_train.astype("float32")/255.0
 x_test = x_test.astype("float32") / 255.0
 
 # Sequential Model
-# modelSeq = keras.Sequential(
-#     [
-#         keras.Input(shape=(32, 32, 3)),
-#         layers.Conv2D(32, 3, padding="valid", activation='relu'),
-#         layers.MaxPooling2D(pool_size=(2, 2)),
-#         layers.Conv2D(64, 3, activation='relu'),
-#         layers.MaxPooling2D(),
-#         layers.Conv2D(128, 3, activation='relu'),
-#         layers.Flatten(),
-#         layers.Dense(64, activation='relu'),
-#         layers.Dense(10),
-#     ]
-# )
+modelSeq = keras.Sequential(
+    [
+        keras.Input(shape=(32, 32, 3)),
+        layers.Conv2D(32, 3, padding="valid", activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(128, 3, activation='relu'),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(10),
+    ]
+)
 
-# print(modelSeq.summary())
+print(modelSeq.summary())
 
-# modelSeq.compile(
-#     loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#     optimizer=keras.optimizers.Adam(learning_rate=3e-4),
-#     metrics=["accuracy"],
-# )
+modelSeq.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(learning_rate=3e-4),
+    metrics=["accuracy"],
+)
 
-# modelSeq.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
-# modelSeq.evaluate(x_test, y_test, batch_size=64, verbose=2)
+modelSeq.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
+modelSeq.evaluate(x_test, y_test, batch_size=64, verbose=2)
 
 # Functional Model
 
 
 def myModelFun():
     inputs = keras.Input(shape=(32, 32, 3))
-    x = layers.Conv2D(32, 3)(inputs)
+    x = layers.Conv2D(
+        32, 3, padding='same', kernel_regularizer=regularizers.l2(0.01),
+    )(inputs)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
     x = layers.MaxPooling2D()(x)
-    x = layers.Conv2D(64, 5)(x)
+    x = layers.Conv2D(
+        64, 5, padding='same', kernel_regularizer=regularizers.l2(0.01),
+    )(x)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
-    x = layers.Conv2D(128, 3)(x)
+    x = layers.Conv2D(
+        128, 3, padding='same', kernel_regularizer=regularizers.l2(0.01),
+    )(x)
     x = layers.BatchNormalization()(x)
     x = keras.activations.relu(x)
     x = layers.Flatten()(x)
-    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dense(
+        64, activation='relu', kernel_regularizer=regularizers.l2(0.01),
+    )(x)
+    x = layers.Dropout(0.5)(x)
     outputs = layers.Dense(10)(x)
     model = keras.Model(inputs=inputs, outputs=outputs)
     return model
@@ -64,5 +73,6 @@ modelFun.compile(
     metrics=["accuracy"],
 )
 
-modelFun.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
+
+modelFun.fit(x_train, y_train, batch_size=64, epochs=150, verbose=2)
 modelFun.evaluate(x_test, y_test, batch_size=64, verbose=2)
